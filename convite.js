33f69f70ac6a9b7e1b21a9c1f3702400e201e140
Thiago Lopes <thiagoohmlopes@gmail.com>
2026-06-17 10:54:40 -0300

    adição da poeira para dispositivos móveis, animações de transição ao clicar no convide fade out depois entra a borda/poeira, mascara, logo e por ultimo o conteudo

--- convite.js
+++ convite.js
@@ -1,13 +1,49 @@
 const btnEntrar = document.getElementById("btnEntrar");
-
 const capa = document.getElementById("capa");
-
 const convite = document.getElementById("convite");
+const envelope = document.getElementById("envelope");
+
+const mascara = document.getElementById("mascara");
+const logo = document.getElementById("logoConvite");
+const conteudo = document.getElementById("conteudoConvite");
+const poeira = document.getElementById("poeiraDourada");
+const borda = document.getElementById("borda");
 
 btnEntrar.addEventListener("click", () => {
 
-    capa.classList.add("hidden");
+    // anima envelope
+    envelope.classList.add("envelope-open");
 
-    convite.classList.remove("hidden");
+    setTimeout(() => {
+
+        // desaparece capa
+        capa.classList.add("fade-out");
+
+        setTimeout(() => {
+
+            // troca de telas
+            capa.classList.add("hidden");
+            convite.classList.remove("hidden");
+
+            poeira.classList.add("poeira-animar");
+            borda.classList.add("borda-animar");
+            // máscara entra primeiro
+            mascara.classList.add("mascara-animar");
+
+            // logo entra depois
+            setTimeout(() => {
+                logo.classList.add("logo-animar");
+            }, 400);
+
+            // conteúdo aparece por último
+            setTimeout(() => {
+
+                conteudo.classList.add("conteudo-animar");
+
+            }, 1000);
+
+        }, 1000);
+
+    }, 700);
 
 });
